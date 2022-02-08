bfda032e0ba950eaec196460758eeaf8126f5e7b
Jose Mena <josemena222@gmail.com>
2022-02-07 20:39:03 -0600

    workshop completed

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Table from './Table';
+
+class App extends React.Component {
+    render(){
+      return (
+        <div classNamne="container"> 
+            <Table />
+        </div>
+      );
+    }
+  
+  }
+
+
+export default App;
---
+++ src/components/TableBody.js
@@ -0,0 +1,26 @@
+const TableBody = () => {
+
+    return (
+        <tbody>
+        <tr>
+            <td>Daniel</td>
+            <td>Full Stack Developer/Musician</td>
+        </tr>
+        <tr>
+            <td>Thomas</td>
+            <td>Senior React-Developer</td>
+        </tr>
+        <tr>
+            <td>Elizabeth</td>
+            <td>BlockChain Developer</td>
+        </tr>
+        <tr>
+            <td>Emma</td>
+            <td>CEO </td>
+        </tr>
+    </tbody>
+
+    );
+
+};
+export default TableBody;
